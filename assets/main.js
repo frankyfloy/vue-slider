075f42fdf997c41234e7471ff7450d6025791cf3
Francesco Sica <fsica171@gmail.com>
2021-03-24 16:39:41 +0100

    carousel

---
+++ assets/main.js
@@ -0,0 +1,37 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+        index : 0,
+        a_IMG : [
+            "assets\\img\\salerno.png",
+            "assets\\img\\salerno2.jpg",
+            "assets\\img\\salerno3.webp",
+            "assets\\img\\salerno4.jpg",
+            "assets\\img\\salerno5.jpg",
+            "assets\\img\\salerno6.jpg",
+            "assets\\img\\salerno7.jpg",
+        ]
+    },
+
+    methods: {
+        next: function () {
+            if (this.index < this.a_IMG.length - 1) {
+                this.index++;
+            }else{
+                this.index = 0;
+            }
+        },
+
+        prev: function () {
+            if (this.index > 0) {
+                this.index--;
+            }else{
+                this.index = this.a_IMG.length - 1;
+            }
+        }
+    }
+
+    // setTimeout(function (){
+    //
+    // },1200);
+});
